b27c9cfc68ba4556ce4e8aa23665a28fadcab199
Jaime Alexander Uribe Uribe <jaimealexanderuribe@gmail.com>
2023-06-21 12:23:25 +0200

    Se crea arquitectura de capas Presentation, Infrastructure y Domain

---
+++ Freetogame/Freetogame/FreetogameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreetogameApp.swift
+//  Freetogame
+//
+//  Created by Jaime Alexander Uribe Uribe - Ceiba Software on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FreetogameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
